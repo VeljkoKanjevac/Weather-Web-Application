20fcbe5c33dcc25a796b2908f7e854400acc8bf5
VeljkoKanjevac <Kanjevacveljko@gmail.com>
2025-02-16 00:58:43 +0100

    *Added admin routes group in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminCheckMiddleware;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::middleware(['auth', AdminCheckMiddleware::class])->prefix('/admin')->group(function () {
+
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
