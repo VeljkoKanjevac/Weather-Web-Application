6c5f3601bfbda8e6f80219342d8d6a627ceb0f7f
VeljkoKanjevac <Kanjevacveljko@gmail.com>
2025-02-16 22:14:39 +0100

    *Routes changed

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::middleware('auth')->group(function () {
     //User rute
 });
@@ -34,9 +35,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::controller(ProfileController::class)->name('profile.')->group(function () {
+
+        Route::get('/profile', 'edit')->name('edit');
+        Route::patch('/profile', 'update')->name('update');
+        Route::delete('/profile', 'destroy')->name('destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
